9b03b20dcf93bfc436732b926017fd9a590fdb8f
ns9489 <ns948966@gmail.com>
2024-08-29 19:49:44 -0500

    Ya funcionan correctamente los botones de los cards

--- ejercicio.js
+++
@@ -1,8 +0,0 @@
-/*3.Dada la cadena “Javascript es genial” recorra la cadena usando ciclo for y muestre por cuantas palabras está
-compuesta la cadena. Como sugerencia se puede basar en el modelo del ejercicio 2 literal d*/
-
-let mensaje = "Javascript es genial"
-
-for (let i = 0; i < mensaje.length; i++) {
-    console.log(mensaje.length)
-}
